7255d6544836030ba91fdc1756716c7aa47fe682
Marwa Hassan <hassanmarwa2023@gmail.com>
2023-11-19 11:17:22 +0500

    small changes

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Str;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::all();
+        return PostResource::collection($post);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostCreateRequest $request)
+    {
+        $post = Post::create(['title'=>$request->title,'sub_title'=>$request->sub_title,'description'=>$request->description,'slug'=>Str::slug($request->title),'lang'=>'en','profile_id'=>3]);
+
+        return  PostResource::make($post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return PostResource::make(Post::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostCreateRequest $request, $id)
+    {
+      
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->sub_title = $request->sub_title;
+        $post->description = $request->description;
+        $post->slug = Str::slug($request->title);
+        $post->save();
+
+        return PostResource::make($post);
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return "delete";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/post',function(){
+//     return response()->json(['post']);
+//     return "post";
+// });
+
+Route::apiResource('/post',PostController::class);
